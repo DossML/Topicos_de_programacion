d64811b68df6352dabc520b3974c1fb0b644c3a7
Doss <112189041+DossML@users.noreply.github.com>
2023-05-09 22:42:09 -0300

    Delete Clase2 directory

--- Clase2/vectorEnteros.c
+++
@@ -1,9 +0,0 @@
-#include "vectorEnteros.h"
-
-void mostrarVector(int* vec, int ce){
-    /*int i;
-    for(i=0; i<ce; i++){
-        printf("\nValor %d: %d", ce, vec[i]);
-    }
-    */
-}
--- Clase2/vectorEnteros.h
+++
@@ -1,6 +0,0 @@
-#ifndef VECTORENTEROS_H_INCLUDED
-#define VECTORENTEROS_H_INCLUDED
-
-void mostrarVector(int*, int);
-
-#endif // VECTORENTEROS_H_INCLUDED
